bedad6be9cd68d3aeb688482a9c5bf25d9089994
敖律风 <x123456789fy@outlook.com>
2023-12-28 21:35:06 +0800

    使用笨方法替代

--- script/lunch.sh
+++ script/lunch.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-java -jar /jbin/server.jar @memorysize.txt @userjvmargs.txt -nogui
+rm -rf /app/server.jar
+cp /jbin/server.jar /app/server.jar
+java -jar /app/server.jar @memorysize.txt @userjvmargs.txt -nogui
+rm -rf /app/server.jar
